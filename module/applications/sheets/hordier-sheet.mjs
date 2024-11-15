0aecbb257e5b49e31cc30d7e9b3266cfb308bf3b
Christophe Laudon <ch.laudon@gmail.com>
2024-11-15 20:47:52 +0100

    :sparkles: Hordier : ajout de l'image de l'acteur

--- module/applications/sheets/hordier-sheet.mjs
+++ module/applications/sheets/hordier-sheet.mjs
@@ -12,6 +12,7 @@ export default class HordierSheet extends RafalesActorSheet {
     classes: ["hordier"],
     position: { width: 670, height: 1000 },
     actions: {
+      editImage: HordierSheet.#onEditImage,
       toggleSheet: HordierSheet.#onToggleSheet,
       createLink: HordierSheet.#onCreateLink,
       deleteLink: HordierSheet.#onDeleteLink,
@@ -60,6 +61,32 @@ export default class HordierSheet extends RafalesActorSheet {
     context.isPlayMode = this.isPlayMode
     context.isEditable = this.isEditable
     return context
+  }
+
+  /**
+   * Handle changing a Document's image.
+   *
+   * @this TenebrisCharacterSheet
+   * @param {PointerEvent} event   The originating click event
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action]
+   * @returns {Promise}
+   * @private
+   */
+  static async #onEditImage(event, target) {
+    const attr = target.dataset.edit
+    const current = foundry.utils.getProperty(this.document, attr)
+    const { img } = this.document.constructor.getDefaultArtwork?.(this.document.toObject()) ?? {}
+    const fp = new FilePicker({
+      current,
+      type: "image",
+      redirectToRoot: img ? [img] : [],
+      callback: (path) => {
+        this.document.update({ [attr]: path })
+      },
+      top: this.position.top + 40,
+      left: this.position.left + 10,
+    })
+    return fp.browse()
   }
 
   /**
