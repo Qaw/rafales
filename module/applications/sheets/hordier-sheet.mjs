d07e02a069e919583a71ab7586b8cb1e07daf5d3
Christophe Laudon <ch.laudon@gmail.com>
2024-11-15 17:18:13 +0100

    :sparkles: Hordier : création d'un lien

--- module/applications/sheets/hordier-sheet.mjs
+++ module/applications/sheets/hordier-sheet.mjs
@@ -5,6 +5,9 @@ export default class HordierSheet extends RafalesActorSheet {
   static DEFAULT_OPTIONS = {
     classes: ["hordier"],
     position: { width: 500, height: 500 },
+    actions: {
+      createLink: HordierSheet.#onCreateLink,
+    },
   }
 
   /** @override */
@@ -23,5 +26,11 @@ export default class HordierSheet extends RafalesActorSheet {
     context.enrichedNotes = await TextEditor.enrichHTML(this.document.system.notes, { async: true })
     context.liens = this.document.system.liens
     return context
+  }
+
+  static #onCreateLink(event, target) {
+    let liens = this.actor.system.liens
+    liens.push({ nom: "Nouveau lien", lien: "" })
+    this.actor.update({ "system.liens": liens })
   }
 }
